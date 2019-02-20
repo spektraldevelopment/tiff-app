3112aed09d642a1750e39050b6ab306a73065705
Boyle, Dave <dave.boyle@investorsgroup.com>
2019-02-20 16:38:55 -0500

    [master] - Roughed in Header and App components.

---
+++ tiff-app/src/components/movies/movies.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Movies extends Component {
+    render() {
+        return(
+            <div>
+                Movies here!
+            </div>
+        );
+    }
+}
+
+export default Movies;
