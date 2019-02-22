3c7a6f7a81086335e619a37689924c307ec71427
Boyle, Dave <dave.boyle@investorsgroup.com>
2019-02-22 16:44:26 -0500

    [master] - Added loading icon to loading screen component.

--- tiff-app/src/components/loading-screen/loading-screen.js
+++ tiff-app/src/components/loading-screen/loading-screen.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import { BeatLoader } from 'react-spinners';
 
 class LoadingScreen extends Component {
     render() {
         return (
-            <div>Loading......</div>
+            <div className="container">
+                <div className="row justify-content-center">
+                    <BeatLoader />
+                </div>
+            </div>
         );
     }
 }
