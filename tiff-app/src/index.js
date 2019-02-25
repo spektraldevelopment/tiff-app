ee59f55f926668be0e676f12d803f020a88df795
Boyle, Dave <dave.boyle@investorsgroup.com>
2019-02-25 16:39:08 -0500

    [static] - Playing around with react-snapshot.

--- tiff-app/src/index.js
+++ tiff-app/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-snapshot';
 import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -7,7 +7,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render((
+render((
     <BrowserRouter>
       <App/>
     </BrowserRouter>
